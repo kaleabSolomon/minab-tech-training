b45cdc6b95148381dd98f7153b788c047c9a8d6f
Kaleab Solomon <kaleabslmn@gmail.com>
2023-05-30 05:38:35 +0300

    configured apollo into the project

--- CountryList/src/main.js
+++ CountryList/src/main.js
@@ -1,5 +1,32 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp, provide, h } from "vue";
+import { DefaultApolloClient } from "@vue/apollo-composable";
+import App from "./App.vue";
+import "./style.css";
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+} from "@apollo/client/core";
+// HTTP connection to the API
+const httpLink = createHttpLink({
+  // You should use an absolute URL here
+  uri: "http://localhost:3020/graphql",
+});
 
-createApp(App).mount('#app')
+// Cache implementation
+const cache = new InMemoryCache();
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+});
+
+const app = createApp({
+  setup() {
+    provide(DefaultApolloClient, apolloClient);
+  },
+
+  render: () => h(App),
+});
+app.mount("#app");
